8a8f72a7306a2efa17be4d8fd5007c9e283e016f
Your Name <mikachupichu@gmail.com>
2023-10-05 20:03:26 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 /* Your Code Here */
 
+// Create an employee record
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // Create employee records
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+  }
+  
+  // Create a time-in event and push it .
+  function createTimeInEvent( dateTime) {
+    const [date, time] = dateTime.split(' ');
+    const timeIn = {
+      type: 'TimeIn',
+      hour: parseInt(time),
+      date: date,
+    };
+    this.timeInEvents.push(timeIn);
+    return this;
+  }
+  
+  // Create a time-out event and push it .
+  function createTimeOutEvent(dateTime) {
+    const [date, time] = dateTime.split(' ');
+    const timeOut = {
+      type: 'TimeOut',
+      hour: parseInt(time),
+      date: date,
+    };
+    this.timeOutEvents.push(timeOut);
+    return this;
+  }
+  
+  // Calculate hours worked on a particular date
+  function hoursWorkedOnDate(date) {
+    const timeArrive = this.timeInEvents.find(event => event.date === date);
+    const timeLeave = this.timeOutEvents.find(event => event.date === date);
+    const hours= (timeLeave.hour - timeArrive.hour) / 100;
+    return hours;
+  }
+  
+  // Calculate wages earned on a particular date.
+  function wagesEarnedOnDate( date) {
+    const hours = hoursWorkedOnDate.call(this, date);
+    const wages = hours * this.payPerHour;
+    return wages;
+  }
+  
+ 
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,5 +77,18 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+// Calculate total pay owed to all employees.
+function calculatePayroll(employees) {
+    const totalPayroll = employees.reduce(function (acc, employee) {
+      return acc + allWagesFor.call(employee);
+    }, 0);
+    return totalPayroll;
+  }
+
+  // Function to find employee by first name.
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
 }
 
